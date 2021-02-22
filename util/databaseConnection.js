f02612711b00cbe4800d0f9346cc268f87c3a14a
sssubik <subikshrestha@gmail.com>
2021-02-22 11:18:44 +0545

    added to gitignore

--- util/databaseConnection.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('eth','root','root',{
-    dialect: 'mysql',
-    host:'localhost',
-    port: 3306
-})
-module.exports = sequelize;
